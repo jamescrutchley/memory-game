1e4b6e66db328b31c6183fb2f9076a841d9e1fa2
James Crutchley <jamescrutchley98@gmail.com>
2023-08-07 20:36:38 +1000

    add and test pexels api

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,32 @@
-import './App.css'
+import './App.css';
+import { useState, useEffect } from 'react';
+import { createClient } from 'pexels';
+
+const client = createClient('V8j8SdiBLLnLIPKIfzZdq7grRnpV4Vofw23V9u1BbG7wb1UsYYpt0Wyy');
 
 function App() {
+  const [photo, setPhoto] = useState(null);
+
+  useEffect(() => {
+    client.photos.show({ id: 1061142 }).then(photoData => {
+      console.log(photoData);
+      setPhoto(photoData);
+    });
+  }, []);
+
+  console.log(photo);
 
   return (
-    <>
-    <p>Memory Game</p>
-    </>
-  )
+    <div>
+      <h1>Collection Photos</h1>
+      {photo && (
+        <div>
+          <h2>{photo.title}</h2>
+          <img src={photo.src.large} alt={photo.alt} />
+        </div>
+      )}
+    </div>
+  );
 }
 
-export default App
+export default App;
